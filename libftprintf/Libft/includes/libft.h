91884f562e71fe83b13ef673ea4c0e9e48918871
mpaincha <mpaincha@student.42.fr>
2016-10-13 22:37:52 +0200

    calcule hex sum arg

--- libftprintf/Libft/includes/libft.h
+++ libftprintf/Libft/includes/libft.h
@@ -65,6 +65,7 @@ int					ft_isprint(int c);
 int					ft_isspace(int c);
 int					ft_isupper(int c);
 char				*ft_itoa(int n);
+char				*ft_itoa_imax(intmax_t n);
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
--- libftprintf/Libft/srcs/ft_itoa.c
+++ libftprintf/Libft/srcs/ft_itoa.c
@@ -36,3 +36,48 @@ char	*ft_itoa(int n)
 	}
 	return (str);
 }
+
+static size_t		ft_intlen_imax(intmax_t n, size_t base)
+{
+	int		i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (base == 10 && n < 0)
+	{
+		n = -n;
+		i++;
+	}
+	while (n != 0)
+	{
+		n = n / base;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa_imax(intmax_t n)
+{
+	char			*str;
+	unsigned int	nb;
+	size_t			len;
+
+	len = ft_intlen_imax(n, 10);
+	nb = (unsigned int)n;
+	str = ft_strnew(len + 1);
+	if (n < 0)
+	{
+		nb = -n;
+		str[0] = '-';
+	}
+	str[len] = '\0';
+	if (n == 0)
+		str[--len] = '0';
+	while (nb != 0)
+	{
+		str[--len] = (nb % 10) + 48;
+		nb = nb / 10;
+	}
+	return (str);
+}
