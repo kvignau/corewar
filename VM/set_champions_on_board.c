8f88d4a386a4ee492b7085e6e3acb61a1b6233c0
Michael CHEVALLIER <mchevall@e2r4p11.42.fr>
2016-10-27 20:09:43 +0200

    set champions on board clean

--- VM/set_champions_on_board.c
+++ VM/set_champions_on_board.c
@@ -12,36 +12,35 @@
 
 #include "corewar.h"
 
-static void	content_to_board(t_elem *champ, unsigned char *board, int *i)
+static void	content_to_board(t_elem *champ, unsigned char *board,
+	t_dbllist *ch_list)
 {
+	int		i;
 	int		j;
 	unsigned char *str;
 
+	i = (((MEM_SIZE) / ch_list->length) *
+		((((t_champ *)(champ->content))->id) - 1));
 	j = 0;
 	str =(unsigned char *)ft_memalloc(3);
 	while (j < (((t_champ *)(champ->content))->size) * 2)
 	{
 		str[0] = (((t_champ *)(champ->content))->content[j]);
 		str[1] = (((t_champ *)(champ->content))->content[j + 1]);
-		board[*i] = (unsigned char)ft_hextoi(str) ;
+		board[i] = (unsigned char)ft_hextoi(str) ;
 		j += 2;
-		*i += 1;
+		i += 1;
 	}
 }
 
 void		set_champions_on_board(t_dbllist *ch_list, unsigned char **board)
 {
 	t_elem	*tmp;
-	int		i;
-
-	i = 0;
 
 	tmp = ch_list->head;
 	while (tmp != NULL)
 	{
-		i = (((MEM_SIZE) / ch_list->length) *
-			((((t_champ *)(tmp->content))->id) - 1));
-		content_to_board(tmp, *board, &i);
+		content_to_board(tmp, *board, ch_list);
 		tmp = tmp->next;
 	}
 }
