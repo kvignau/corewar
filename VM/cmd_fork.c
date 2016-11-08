fc89bf80aaf9d316e2cfe8df3c4945b9812740fd
Michael CHEVALLIER <mchevall@e3r4p4.42.fr>
2016-11-08 19:15:24 +0100

    fork is done

--- VM/cmd_fork.c
+++ VM/cmd_fork.c
@@ -20,7 +20,6 @@ void		cmd_fork(unsigned char *board, t_proc *c_proc, t_dbllist *pr_list)
 	if (c_proc->ctp == 800)
 	{
 		value = bit_cat(board, c_proc, 1, 2);
-		ft_printf("value :%u\n", value);
 		ft_lstdbladd_head(pr_list, (t_proc *)c_proc, sizeof(t_proc));
 		tmp = pr_list->head;
 		((t_proc *)(tmp->content))->pc = (c_proc->pc + (value % IDX_MOD));
