d1f1fcf2d34c2e7d90bd64b5d40d6aab713e6a76
Michael CHEVALLIER <mchevall@e2r5p5.42.fr>
2016-11-09 13:37:22 +0100

    cmd lfork

---
+++ VM/cmd_lfork.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_lfork.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchevall <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/07 15:12:06 by mchevall          #+#    #+#             */
+/*   Updated: 2016/11/07 15:12:38 by mchevall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+void		cmd_lfork(unsigned char *board, t_proc *c_proc, t_dbllist *pr_list)
+{
+	unsigned int		value;
+	t_elem				*tmp;
+
+	if (c_proc->ctp == 800)
+	{
+		value = bit_cat(board, c_proc, 1, 2);
+		ft_lstdbladd_head(pr_list, (t_proc *)c_proc, sizeof(t_proc));
+		tmp = pr_list->head;
+		next_pc(3, c_proc, board);
+		((t_proc *)(tmp->content))->pc = (c_proc->pc + (value % IDX_MOD));
+		((t_proc *)(tmp->content))->i = (c_proc->i + (value % IDX_MOD));
+		c_proc->ctp = 0;
+	}
+	else
+		c_proc->ctp += 1;
+}
