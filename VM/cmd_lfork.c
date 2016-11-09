f27e77de9f6231cffeb56d7947b284605527bcac
Michael CHEVALLIER <mchevall@e2r5p5.42.fr>
2016-11-09 13:41:01 +0100

    patch lfork

--- VM/cmd_lfork.c
+++ VM/cmd_lfork.c
@@ -17,14 +17,14 @@ void		cmd_lfork(unsigned char *board, t_proc *c_proc, t_dbllist *pr_list)
 	unsigned int		value;
 	t_elem				*tmp;
 
-	if (c_proc->ctp == 800)
+	if (c_proc->ctp == 1000)
 	{
 		value = bit_cat(board, c_proc, 1, 2);
 		ft_lstdbladd_head(pr_list, (t_proc *)c_proc, sizeof(t_proc));
 		tmp = pr_list->head;
 		next_pc(3, c_proc, board);
-		((t_proc *)(tmp->content))->pc = (c_proc->pc + (value % IDX_MOD));
-		((t_proc *)(tmp->content))->i = (c_proc->i + (value % IDX_MOD));
+		((t_proc *)(tmp->content))->pc = (c_proc->pc + (value));
+		((t_proc *)(tmp->content))->i = (c_proc->i + (value));
 		c_proc->ctp = 0;
 	}
 	else
