4a83e7bf7d1e05e389cc0cb4f9a9e659c6408f6f
Kevin VIGNAU <kvignau@e2r5p6.42.fr>
2016-11-09 15:00:49 +0100

    cmd_ld ok pour indirect revoir direct

--- VM/cmd_ld.c
+++ VM/cmd_ld.c
@@ -21,17 +21,19 @@ void	cmd_ld(unsigned char *board, t_proc *c_proc)
 		if (board[(c_proc->i + 1) % MEM_SIZE] == 0x90)
 		{
 				id = bit_cat(board, c_proc, 2, 4);
-				// c_proc->r[(int)(board[c_proc->i + 6 % MEM_SIZE]) - 1] = board[(c_proc->pc + (id % IDX_MOD))];
-				// ft_printf("indirect %d\n", c_proc->pc);
-				// ft_printf("TEST DE OUF indirect %x\n", c_proc->pc + (id % IDX_MOD));
+		
+				//PROB AVEC LES DIRECT ...
+
+				c_proc->r[(int)(board[c_proc->i + 6 % MEM_SIZE]) - 1] = bit_cat(board, c_proc, (id % IDX_MOD), 4);
+				
+				// ft_printf("VERIF REG : %x\n", c_proc->r[(int)(board[c_proc->i + 6 % MEM_SIZE]) - 1]);
+				
 				next_pc(7, c_proc, board);
 		}
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0xd0)
 		{
 				id = bit_cat(board, c_proc, 2, 2);
-				c_proc->r[(int)(board[c_proc->i + 4 % MEM_SIZE] - 1)] = (int)(c_proc->pc + (id % IDX_MOD));
-				// ft_printf("direct %d\n", c_proc->pc);
-				// ft_printf("TEST DE OUF direct %x\n", c_proc->pc + (id % IDX_MOD));
+				c_proc->r[(int)(board[c_proc->i + 4 % MEM_SIZE]) - 1] = bit_cat(board, c_proc, (id % IDX_MOD), 4);
 				next_pc(5, c_proc, board);
 		}
 		if (c_proc->pc + (id % IDX_MOD) == 0)
