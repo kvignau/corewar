29c5d8151ffc0ecf9a8dc8be0fb2cd0a19d85168
Kevin VIGNAU <kvignau@e2r5p6.42.fr>
2016-11-09 16:27:16 +0100

    cmd_ld ok

--- VM/cmd_ld.c
+++ VM/cmd_ld.c
@@ -21,13 +21,13 @@ void	cmd_ld(unsigned char *board, t_proc *c_proc)
 		if (board[(c_proc->i + 1) % MEM_SIZE] == 0x90)
 		{
 				id = bit_cat(board, c_proc, 2, 4);
-				c_proc->r[(int)(board[c_proc->i + 6 % MEM_SIZE]) - 1] = bit_cat(board, c_proc, 2, 4);
+				c_proc->r[(int)(board[c_proc->i + (6 % MEM_SIZE)]) - 1] = bit_cat(board, c_proc, 2, 4);
 				next_pc(7, c_proc, board);
 		}
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0xd0)
 		{
 				id = bit_cat(board, c_proc, 2, 2);
-				c_proc->r[(int)(board[c_proc->i + 4 % MEM_SIZE]) - 1] = bit_cat(board, c_proc, (id % IDX_MOD), 4);
+				c_proc->r[(int)(board[c_proc->i + (4 % MEM_SIZE)]) - 1] = bit_cat(board, c_proc, (id % IDX_MOD), 4);
 				next_pc(5, c_proc, board);
 		}
 		if (c_proc->pc + (id % IDX_MOD) == 0)
