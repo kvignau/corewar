66d9729829be9d698be370668336cc5289ef372d
michaelchevallier <michael.chevallier@gmail.com>
2016-11-09 02:00:52 +0100

    fork fix

--- VM/cmd_fork.c
+++ VM/cmd_fork.c
@@ -22,9 +22,9 @@ void		cmd_fork(unsigned char *board, t_proc *c_proc, t_dbllist *pr_list)
 		value = bit_cat(board, c_proc, 1, 2);
 		ft_lstdbladd_head(pr_list, (t_proc *)c_proc, sizeof(t_proc));
 		tmp = pr_list->head;
+		next_pc(3, c_proc, board);
 		((t_proc *)(tmp->content))->pc = (c_proc->pc + (value % IDX_MOD));
 		((t_proc *)(tmp->content))->i = (c_proc->i + (value % IDX_MOD));
-		next_pc(3, c_proc, board);
 		c_proc->ctp = 0;
 	}
 	else
