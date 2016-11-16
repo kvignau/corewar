5245fd9251b328957f50296b9c54f510fddfd23f
Kevin VIGNAU <kvignau@e2r6p3.42.fr>
2016-11-16 14:04:11 +0100

    sti ok

--- VM/cmd_sti.c
+++ VM/cmd_sti.c
@@ -12,145 +12,85 @@
 
 #include "corewar.h"
 
-static unsigned int		get_arg_value(unsigned char *board, t_proc *c_proc, int *type, int arg_nb)
+static void				add_dir_reg(unsigned char *board, t_proc *c_proc)
 {
-	if (type[arg_nb - 1] == REG)
-		return(isreg(board, c_proc, type, arg_nb));
-	else if (type[arg_nb - 1] == DIR)
-		return (isdir(board, c_proc, type, arg_nb));
-	else if (type[arg_nb - 1] == IND)
-		return (isind(board, c_proc, type, arg_nb));
-	else
-		return (0);
+	unsigned int		add;
+	unsigned int		id;
+	int					reg_nb;
+	int					reg_nb2;
+
+	add = 0;
+	id = 0;
+	reg_nb = 0;
+	reg_nb2 = 0;
+	reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
+	reg_nb2 = board[(c_proc->i + 5) % MEM_SIZE] - 1;
+	id = bit_cat(board, c_proc, 3, 2);
+	add = (c_proc->r[reg_nb2] + bit_cat(board, c_proc, id + 2, 2)) % IDX_MOD;
+	sti_result(board, c_proc, reg_nb, add);
 }
 
-static int				valid_opc(unsigned char *board, t_proc *c_proc)
+static void				add_dir_ind(unsigned char *board, t_proc *c_proc)
 {
-	int		cmp;
+	unsigned int		add;
+	unsigned int		id;
+	int					reg_nb;
 
-	cmp = board[(c_proc->i + 1) % MEM_SIZE];
-	if (cmp == 0x54 || cmp == 0x58 || cmp == 0x64 || cmp == 0x68 ||
-		cmp == 0x74 || cmp == 0x78)
-		return (1);
-	return (0);
+	add = 0;
+	id = 0;
+	reg_nb = 0;
+	reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
+	id = bit_cat(board, c_proc, 3, 2);
+	add = (bit_cat(board, c_proc, 5, 2) + bit_cat(board, c_proc, id + 2, 2)) % IDX_MOD;
+	sti_result(board, c_proc, reg_nb, add);
+}
+
+static void				add_ind_ind(unsigned char *board, t_proc *c_proc)
+{
+	unsigned int		add;
+	int					reg_nb;
+
+	add = 0;
+	reg_nb = 0;
+	add = bit_cat(board, c_proc, 3, 2) + bit_cat(board, c_proc, 5, 2);
+	reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
+	sti_result(board, c_proc, reg_nb, add);
+}
+
+static void				add_reg_ind(unsigned char *board, t_proc *c_proc)
+{
+	unsigned int		add;
+	int					reg_nb;
+	int					reg_nb2;
+
+	add = 0;
+	reg_nb = 0;
+	reg_nb2 = 0;
+	reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
+	reg_nb2 = board[(c_proc->i + 3) % MEM_SIZE] - 1;
+	add = c_proc->r[reg_nb2] + bit_cat(board, c_proc, 4, 2);
+	sti_result(board, c_proc, reg_nb, add);
 }
 
 void					cmd_sti(unsigned char *board, t_proc *c_proc)
 {
-	unsigned int		result;
-	unsigned int		add;
-	int					reg_nb;
-	int					reg_nb2;
-	int					reg_nb3;
-	int					i;
-	int					id;
-	int					*type;
-	int					first_arg;
-
-	result = 0;
-	add = 0;
-	i = 0;
-	id = 0;
-	reg_nb = 0;
-	reg_nb2 = 0;
-	reg_nb3 = 0;
-	// if (c_proc->ctp == 25)
+	if (c_proc->ctp == 25)
 	{
-		type = get_type(board, c_proc);
-		// if (valid_opc(board, c_proc) == 1)
-		// {
-		// }
 		if (board[(c_proc->i + 1) % MEM_SIZE] == 0x68)
-		{
-			add = bit_cat(board, c_proc, 3, 2) + bit_cat(board, c_proc, 5, 2);
-			reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
-			result = c_proc->r[reg_nb] >> (8 * (REG_SIZE - 1));
-			while (i < REG_SIZE)
-			{
-				board[(c_proc->i + (add + i)) % MEM_SIZE] = result;
-				result = c_proc->r[reg_nb] >> ((8 * (REG_SIZE - 1)) - (8 * (i + 1)));
-				i++;
-			}
-		}
+			add_ind_ind(board, c_proc);
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0x58)
-		{
-			reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
-			reg_nb2 = board[(c_proc->i + 3) % MEM_SIZE] - 1;
-			add = c_proc->r[reg_nb2] + bit_cat(board, c_proc, 4, 2);
-			result = c_proc->r[reg_nb] >> (8 * (REG_SIZE - 1));
-			while (i < REG_SIZE)
-			{
-				board[(c_proc->i + (add + i)) % MEM_SIZE] = result;
-				result = c_proc->r[reg_nb] >> ((8 * (REG_SIZE - 1)) - (8 * (i + 1)));
-				i++;
-			}
-		}
+			add_reg_ind(board, c_proc);
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0x64)
-		{
-			reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
-			reg_nb2 = board[(c_proc->i + 5) % MEM_SIZE] - 1;
-			add = c_proc->r[reg_nb2] + bit_cat(board, c_proc, 3, 2);
-			result = c_proc->r[reg_nb] >> (8 * (REG_SIZE - 1));
-			while (i < REG_SIZE)
-			{
-				board[(c_proc->i + (add + i)) % MEM_SIZE] = result;
-				result = c_proc->r[reg_nb] >> ((8 * (REG_SIZE - 1)) - (8 * (i + 1)));
-				i++;
-			}
-		}
+			add_ind_reg(board, c_proc);
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0x54)
-		{
-			reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
-			
-			reg_nb2 = board[(c_proc->i + 3) % MEM_SIZE] - 1;
-			reg_nb3 = board[(c_proc->i + 4) % MEM_SIZE] - 1;
-			
-			add = c_proc->r[reg_nb2] + c_proc->r[reg_nb3];
-			result = c_proc->r[reg_nb] >> (8 * (REG_SIZE - 1));
-			while (i < REG_SIZE)
-			{
-				board[(c_proc->i + (add + i)) % MEM_SIZE] = result;
-				result = c_proc->r[reg_nb] >> ((8 * (REG_SIZE - 1)) - (8 * (i + 1)));
-				i++;
-			}
-		}
+			add_reg_reg(board, c_proc);
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0x74)
-		{
-			reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
-			reg_nb2 = board[(c_proc->i + 5) % MEM_SIZE] - 1;
-
-			id = bit_cat(board, c_proc, 3, 2);
-			
-			add = (c_proc->r[reg_nb2] + bit_cat(board, c_proc, id + 2, 2)) % IDX_MOD;
-
-			result = c_proc->r[reg_nb] >> (8 * (REG_SIZE - 1));
-			while (i < REG_SIZE)
-			{
-				board[(c_proc->i + (add + i)) % MEM_SIZE] = result;
-				result = c_proc->r[reg_nb] >> ((8 * (REG_SIZE - 1)) - (8 * (i + 1)));
-				i++;
-			}
-		}
+			add_dir_reg(board, c_proc);
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0x78)
-		{
-			reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
-
-			id = bit_cat(board, c_proc, 3, 2);
-			
-			add = (bit_cat(board, c_proc, 5, 2) + bit_cat(board, c_proc, id + 2, 2)) % IDX_MOD;
-
-			result = c_proc->r[reg_nb] >> (8 * (REG_SIZE - 1));
-			while (i < REG_SIZE)
-			{
-				board[(c_proc->i + (add + i)) % MEM_SIZE] = result;
-				result = c_proc->r[reg_nb] >> ((8 * (REG_SIZE - 1)) - (8 * (i + 1)));
-				i++;
-			}
-		}
-		
-		next_pc(get_cmd_size(type, 2, 3), c_proc, board);
+			add_dir_ind(board, c_proc);
+		next_pc(get_cmd_size(get_type(board, c_proc), 2, 3), c_proc, board);
 		c_proc->ctp = 0;
 	}
-	// else
+	else
 		c_proc->ctp += 1;
 }
---
+++ VM/opt_sti.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opt_sti.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvignau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/16 13:55:27 by kvignau           #+#    #+#             */
+/*   Updated: 2016/11/16 13:55:31 by kvignau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+void					sti_result(unsigned char *board, t_proc *c_proc, int reg_nb, unsigned int add)
+{
+	int					i;
+	unsigned int		result;
+
+	i = 0;
+	result = 0;
+	result = c_proc->r[reg_nb] >> (8 * (REG_SIZE - 1));
+	while (i < REG_SIZE)
+	{
+		board[(c_proc->i + (add + i)) % MEM_SIZE] = result;
+		result = c_proc->r[reg_nb] >> ((8 * (REG_SIZE - 1)) - (8 * (i + 1)));
+		i++;
+	}
+}
+
+void					add_ind_reg(unsigned char *board, t_proc *c_proc)
+{
+	unsigned int		add;
+	int					reg_nb;
+	int					reg_nb2;
+
+	add = 0;
+	reg_nb = 0;
+	reg_nb2 = 0;
+	reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
+	reg_nb2 = board[(c_proc->i + 5) % MEM_SIZE] - 1;
+	add = c_proc->r[reg_nb2] + bit_cat(board, c_proc, 3, 2);
+	sti_result(board, c_proc, reg_nb, add);
+}
+
+void					add_reg_reg(unsigned char *board, t_proc *c_proc)
+{
+	unsigned int		add;
+	int					reg_nb;
+	int					reg_nb2;
+	int					reg_nb3;
+
+	add = 0;
+	reg_nb = 0;
+	reg_nb2 = 0;
+	reg_nb3 = 0;
+	reg_nb = board[(c_proc->i + 2) % MEM_SIZE] - 1;
+	reg_nb2 = board[(c_proc->i + 3) % MEM_SIZE] - 1;
+	reg_nb3 = board[(c_proc->i + 4) % MEM_SIZE] - 1;
+	add = c_proc->r[reg_nb2] + c_proc->r[reg_nb3];
+	sti_result(board, c_proc, reg_nb, add);
+}
