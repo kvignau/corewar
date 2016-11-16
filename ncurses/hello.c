ec751b1ea7ca182a95671f20ac83e2f6fc573dd8
Alexandre FOLLIN <afollin@e2r10p17.42.fr>
2016-11-16 17:12:44 +0100

    ncurses 1.1

--- ncurses/hello.c
+++ ncurses/hello.c
@@ -29,15 +29,15 @@ int		set_up_ncurses(WINDOW *windows[])
 {
 	WINDOW		*gauche;
 	WINDOW		*droite;
-	unsigned char *board;
+	unsigned char *board_empty;
 	int i;
 
-	board = (unsigned char *)malloc(sizeof(unsigned char) * 4096);
+	board_empty = (unsigned char *)malloc(sizeof(unsigned char) * 4096);
 	i = -1;
 	while (++i < 4096){
-		board[i] = 0;
+		board_empty[i] = 0;
 	}
-		
+
 	initscr();
 	noecho();
 	gauche= subwin(stdscr, 66, 195, 0, 0);// Créé une fenêtre de 66 lignes et de 195 colonnes en 0, 0
@@ -46,22 +46,14 @@ int		set_up_ncurses(WINDOW *windows[])
 	box(gauche, ACS_VLINE, ACS_HLINE);// contour de la box
 	box(droite, ACS_VLINE, ACS_HLINE);
 	
-	while (1)
-	{
-		print_board(gauche, board);
 
-		mvwprintw(droite, 1, 2, "Ceci est la fenetre de droite");	
-		wrefresh(droite);
+	print_board(gauche, board_empty);
 
-		if (getch() == ' ')
-			break ;
-	}
+	mvwprintw(droite, 1, 2, "Ceci est la fenetre de droite");	
+	wrefresh(droite);
+
 	windows[0] = gauche;
 	windows[1] = droite;
-	// endwin();
-	
-	// free(gauche);
-	// free(droite);
 	return (0);
 }
 
@@ -75,9 +67,9 @@ int main(void) {
 	set_up_ncurses(windows);
 
 	board = (unsigned char *)malloc(sizeof(unsigned char) * 4096);
-	board[0] = 9;
-	board[1] = 0x42;
-	board[2] = 10;
+	board[0] = 3;
+	board[1] = 0x84;
+	board[2] = 31;
 	board[3] = 1;
 	i = -1;
 	while (++i < 4092){
