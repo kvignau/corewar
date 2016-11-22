ae84c0d0beb4728db32abd8c9bfadd48c41f0515
Kevin VIGNAU <kvignau@e3r4p4.42.fr>
2016-11-22 15:51:20 +0100

    get_type bit

--- VM/get_type.c
+++ VM/get_type.c
@@ -12,40 +12,30 @@
 
 #include "corewar.h"
 
-static int		get_arg_type(char *arg)
+static int		get_arg_type(char arg)
 {
-	if (ft_strcmp((char *)arg, "01") == 0)
+	if (arg == 0x1)
 		return (REG);
-	else if (ft_strcmp((char *)arg, "10") == 0)
+	else if (arg == 0x2)
 		return (DIR);
-	else if (ft_strcmp((char *)arg, "11") == 0)
+	else if (arg == 0x3)
 		return (IND);
 	return (0);
 }
 
 int				*get_type(unsigned char *board, t_proc *c_proc)
 {
-	char		*bitcode;
-	char		*first_arg;
-	char		*sec_arg;
-	char		*third_arg;
+	char		arg1;
+	char		arg2;
+	char		arg3;
 	int			*type;
 
-	first_arg = (char *)ft_memalloc(sizeof(char) * 4);
-	sec_arg = (char *)ft_memalloc(sizeof(char) * 4);
-	third_arg = (char *)ft_memalloc(sizeof(char) * 4);
-	type = (int *)ft_memalloc(sizeof(int) * 3);
-	bitcode = ft_itoabase_imax(board[(c_proc->i + 1) % MEM_SIZE], 2);
-	while (ft_strlen(bitcode) < 8)
-		bitcode = ft_strjoinandfree("0", bitcode, 2);
-	first_arg[0] = bitcode[0];
-	first_arg[1] = bitcode[1];
-	sec_arg[0] = bitcode[2];
-	sec_arg[1] = bitcode[3];
-	third_arg[0] = bitcode[4];
-	third_arg[1] = bitcode[5];
-	type[0] = get_arg_type(first_arg);
-	type[1] = get_arg_type(sec_arg);
-	type[2] = get_arg_type(third_arg);
+	type = (int *)malloc(sizeof(int) * 3);
+	arg1 = board[(c_proc->i + 1) % MEM_SIZE] >> 6 & 0x3;
+	arg2 = board[(c_proc->i + 1) % MEM_SIZE] >> 4 & 0x3;
+	arg3 = board[(c_proc->i + 1) % MEM_SIZE] >> 2 & 0x3;
+	type[0] = get_arg_type(arg1);
+	type[1] = get_arg_type(arg2);
+	type[2] = get_arg_type(arg3);
 	return (type);
 }
