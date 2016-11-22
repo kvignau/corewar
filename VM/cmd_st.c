d0cf40e157d05fe94bf75e266366e12dac469807
Kevin VIGNAU <kvignau@e3r4p4.42.fr>
2016-11-22 18:54:16 +0100

    st v2

--- VM/cmd_st.c
+++ VM/cmd_st.c
@@ -12,65 +12,65 @@
 
 #include "corewar.h"
 
-static void		next(unsigned char *board, t_proc *c_proc, int nb, int verbose)
+static void		result_st(unsigned char *board, t_proc *c_proc, int reg_nb, int id)
 {
-	if (verbose == 1)
-		cmd_verbose(board, c_proc, nb);
-	next_pc(nb, c_proc, board);
+	int				i;
+	unsigned int	result;
+
+	i = 0;
+	result = c_proc->r[reg_nb] >> 24;
+	while (i < REG_SIZE)
+	{
+		board[(c_proc->i + (id + i)) % MEM_SIZE] = result;
+		result = c_proc->r[reg_nb] >> (24 - (8 * (i + 1)));
+		i++;
+	}
+}
+
+static void		reg_ind(unsigned char *board, t_proc *c_proc, int verbose)
+{
+	int				id;
+	int				reg_nb;
+
+	id = bit_cat(board, c_proc, 3, 2);
+	reg_nb = (int)((board[(c_proc->i + 2) % MEM_SIZE] - 1));
+	if (reg_nb <= REG_NUMBER || reg_nb >= 0)
+	{
+		if (verbose == 1)
+			ft_printf("P% 5d | st r%d %d\n", c_proc->pid, reg_nb + 1,
+				(short int)id);
+		result_st(board, c_proc, reg_nb, id);
+	}
+}
+
+static void		reg_reg(unsigned char *board, t_proc *c_proc, int verbose)
+{
+	int				reg_nb;
+	int				reg_nb2;
+
+	reg_nb = (int)(board[(c_proc->i + 3) % MEM_SIZE]) - 1;
+	reg_nb2 = (int)(board[(c_proc->i + 2) % MEM_SIZE]) - 1;
+	if (reg_nb <= REG_NUMBER || reg_nb >= 0 || reg_nb2 <= REG_NUMBER || reg_nb2 >= 0)
+	{
+		if (verbose == 1)
+		ft_printf("P% 5d | st r%d %d\n", c_proc->pid, reg_nb2 + 1,
+			reg_nb + 1);
+		c_proc->r[reg_nb] = c_proc->r[reg_nb2];
+	}
 }
 
 void	cmd_st(unsigned char *board, t_proc *c_proc, t_cor *core)
 {
-	unsigned int	id;
-	unsigned char	result;
-	int				i;
-	int				reg_nb;
-	int				reg_nb2;
-
+	int				cmd_size;
+	
 	if (c_proc->ctp == 5)
 	{
-		i = 0;
-		result = 0;
+		cmd_size = get_cmd_size(get_type(board, c_proc), 4, 2);
 		if (board[(c_proc->i + 1) % MEM_SIZE] == 0x70)
-		{
-			id = bit_cat(board, c_proc, 3, 2);
-			reg_nb = (int)((board[(c_proc->i + 2) % MEM_SIZE] - 1));
-			if (reg_nb > 15 || reg_nb < 0)
-			{
-				next(board, c_proc, get_cmd_size(get_type(board, c_proc), 4, 2), core->options.verbose);
-				return ;
-			}
-			if (core->options.verbose == 1)
-				ft_printf("P% 5d | st r%d %d\n", c_proc->pid, reg_nb + 1, (short int)id);
-		}
+			reg_ind(board, c_proc, core->options.verbose);
 		else if (board[(c_proc->i + 1) % MEM_SIZE] == 0x50)
-		{
-			reg_nb = (int)(board[(c_proc->i + 3) % MEM_SIZE]) - 1;
-			reg_nb2 = (int)(board[(c_proc->i + 2) % MEM_SIZE]) - 1;
-			if (reg_nb > 15 || reg_nb < 0 || reg_nb2 > 15 || reg_nb2 < 0)
-			{
-				next(board, c_proc, get_cmd_size(get_type(board, c_proc), 4, 2), core->options.verbose);
-				return ;
-			}
-			if (core->options.verbose == 1)
-				ft_printf("P% 5d | st r%d %d\n", c_proc->pid, reg_nb2 + 1,
-					reg_nb + 1);
-			c_proc->r[reg_nb] = c_proc->r[reg_nb2];
-		}
-		if (board[(c_proc->i + 1) % MEM_SIZE] == 0x70)
-		{
-			reg_nb2 = get_cmd_size(get_type(board, c_proc), 4, 2);
-			result = c_proc->r[reg_nb] >> 24;
-			while (i < REG_SIZE)
-			{
-				board[(c_proc->i + (id + i)) % MEM_SIZE] = result;
-				result = c_proc->r[reg_nb] >> (24 - (8 * (i + 1)));
-				i++;
-			}
-			next(board, c_proc, reg_nb2, core->options.verbose);
-		}
-		else
-			next(board, c_proc, get_cmd_size(get_type(board, c_proc), 4, 2), core->options.verbose);
+			reg_reg(board, c_proc, core->options.verbose);
+		next(board, c_proc, cmd_size, core->options.verbose);
 		c_proc->ctp = 1;
 	}
 	else
