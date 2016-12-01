12ad2e1c914cf131fa938e0a56dcd62e9b3e8790
Kevin VIGNAU <kvignau@e2r8p4.42.fr>
2016-12-01 16:31:36 +0100

    afofo champion

--- VM/cmd_xor.c
+++ VM/cmd_xor.c
@@ -27,22 +27,21 @@ void		cmd_xor(unsigned char *board, t_proc *c_proc, t_cor *core)
 	{
 		arg1 = get_arg_val((o_code & 0b11000000) >> 6, board, c_proc, i);
 		arg2 = get_arg_val((o_code & 0b00110000) >> 4, board, c_proc, i);
-		if ((o_code & 0b00001100) >> 2 == 0b01 && (o_code & 0b00000011) == 0)
+		if ((o_code & 0b00001100) >> 2 == 0b01)
 			r_dest = get_register_name(board, c_proc, i);
 		else
 			i[1] = -1;
 		if (i[1] == 0)
 		{
-			if (core->options.verbose == 1)
-			{
-				ft_printf("P% 5d | xor %d %d r%d\n", c_proc->pid, arg1, arg2,
-					r_dest);
-			}
 			c_proc->r[r_dest - 1] = arg1 ^ arg2;
 			c_proc->carry = c_proc->r[r_dest - 1] == 0 ? 1 : 0;
 		}
 		if (core->options.verbose == 1)
+		{
+			ft_printf("P% 5d | xor %d %d r%d\n", c_proc->pid, arg1, arg2,
+					r_dest);
 			cmd_verbose(board, c_proc, (get_cmd_size(get_type(board, c_proc), 4, 3)));
+		}
 		c_proc->c_cmd = 0;
 		next_pc(get_cmd_size(get_type(board, c_proc), 4, 3), c_proc, board);
 		c_proc->ctp = 1;
