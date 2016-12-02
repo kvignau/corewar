0462bc2ffb7532a64b6501ca98172325dd2da6ec
Michael CHEVALLIER <mchevall@e2r9p5.42.fr>
2016-12-02 07:34:05 +0100

    norme .h

--- VM/corewar.h
+++ VM/corewar.h
@@ -95,104 +95,119 @@ typedef struct		s_proc
 **********************  Display  **********************
 */
 
-int			corewar_usage(int argc);
-void		ft_print_memory(unsigned char *ptr, int memsize);
-void		intro(t_dbllist *champ_list);
+int					corewar_usage(int argc);
+void				ft_print_memory(unsigned char *ptr, int memsize);
+void				intro(t_dbllist *champ_list);
 
 /*
 **********************  Options  **********************
 */
 
-int			options_checkers(int argc, char **argv, t_options *options,
+int					options_checkers(int argc, char **argv, t_options *options,
 	t_dbllist *champ_list);
-void		cmd_verbose(unsigned char *board, t_proc *c_proc, int offset);
-void		cmd_verbose_sti(unsigned char *board, t_proc *c_proc, int p1, int p2);
-void		cmd_verbose_st(int pid, int reg_nb, int id);
-void		cmd_verbose_ld(int pid, int reg_nb, int id);
-void		cmd_verbose_zjmp_live(char *cmd, int pid, int id);
+void				cmd_verbose(unsigned char *board, t_proc *c_proc,
+	int offset);
+void				cmd_verbose_sti(unsigned char *board, t_proc *c_proc,
+	int p1, int p2);
+void				cmd_verbose_st(int pid, int reg_nb, int id);
+void				cmd_verbose_ld(int pid, int reg_nb, int id);
+void				cmd_verbose_zjmp_live(char *cmd, int pid, int id);
 
 /*
 **********************  Checkers  **********************
 */
 
-int			champions_maker(t_dbllist *champ_list, t_options *options,
+int					champions_maker(t_dbllist *champ_list, t_options *options,
 	t_var *var);
 
 /*
 **********************  Utils  **********************
 */
 
-uintmax_t		ft_hextoi(unsigned char *str);
-char			*ft_hextoa(int *str, int size);
-unsigned char	*get_content(int *str, int size);
-int				reverse_byte(int buf, int readv);
-int				isreg(unsigned char *board, t_proc *c_proc, int *type, int arg_nb);
-int				isdir(unsigned char *board, t_proc *c_proc, int *type, int arg_nb);
-int				isind(unsigned char *board, t_proc *c_proc, int *type, int arg_nb);
+uintmax_t			ft_hextoi(unsigned char *str);
+char				*ft_hextoa(int *str, int size);
+unsigned char		*get_content(int *str, int size);
+int					reverse_byte(int buf, int readv);
+int					isreg(unsigned char *board, t_proc *c_proc, int *type,
+	int arg_nb);
+int					isdir(unsigned char *board, t_proc *c_proc, int *type,
+	int arg_nb);
+int					isind(unsigned char *board, t_proc *c_proc, int *type,
+	int arg_nb);
 
 /*
 **********************  Memory Setter  **********************
 */
 
-unsigned char	*memory(void);
-void			init_board(t_dbllist *ch_list, t_dbllist *pr_list, t_cor *core);
+unsigned char		*memory(void);
+void				init_board(t_dbllist *ch_list, t_dbllist *pr_list,
+	t_cor *core);
 
 /*
 **********************  Commands **********************
 */
 
-
-int				get_cmd_size(int *type, int label_size, int nb_param);
-int				*get_type(unsigned char *board, t_proc *c_proc);
-int				get_register_name(unsigned char *board, t_proc *c_proc, int i[]);
-int				get_arg_val(int oc_trunc, unsigned char *board, t_proc *c_proc, int i[]);
-unsigned int	bit_cat(unsigned char *board, t_proc *c_proc, int start, int size);
-int				bit_cat_no_mod(unsigned char *board, t_proc *c_proc, int start, int size);
-void			next(unsigned char *board, t_proc *c_proc, int nb, int verbose);
-void			next_pc(int counter_offset, t_proc *c_proc, unsigned char *board);
-void			cmd_live(unsigned char *board, t_proc *c_proc, t_cor *core,
+int					get_cmd_size(int *type, int label_size, int nb_param);
+int					*get_type(unsigned char *board, t_proc *c_proc);
+int					get_register_name(unsigned char *board, t_proc *c_proc,
+	int i[]);
+int					get_arg_val(int oc_trunc, unsigned char *board,
+	t_proc *c_proc, int i[]);
+unsigned int		bit_cat(unsigned char *board, t_proc *c_proc, int start,
+	int size);
+int					bit_cat_no_mod(unsigned char *board, t_proc *c_proc,
+	int start, int size);
+void				next(unsigned char *board, t_proc *c_proc,
+	int nb, int verbose);
+void				next_pc(int counter_offset, t_proc *c_proc,
+	unsigned char *board);
+void				cmd_live(unsigned char *board, t_proc *c_proc, t_cor *core,
 	t_dbllist *ch_list);
-void			cmd_ld(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_st(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_add(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_sub(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_and(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_or(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_xor(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_zjmp(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_ldi(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_sti(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			sti_result(t_cor *core, t_proc *c_proc, int reg_nb, unsigned int add);
-int				add_ind_reg(unsigned char *board, t_proc *c_proc, int v, int *reg_nb);
-int				add_reg_reg(unsigned char *board, t_proc *c_proc, int v, int *reg_nb);
-void			cmd_fork(unsigned char *board, t_proc *c_proc, t_dbllist *pr_list, t_cor *core);
-void			cmd_lld(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_lldi(unsigned char *board, t_proc *c_proc, t_cor *core);
-void			cmd_lfork(unsigned char *board, t_proc *c_proc, t_dbllist *pr_list, t_cor *core);
-void			cmd_aff(unsigned char *board, t_proc *c_proc, t_cor *core);
-
+void				cmd_ld(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_st(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_add(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_sub(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_and(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_or(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_xor(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_zjmp(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_ldi(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_sti(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				sti_result(t_cor *core, t_proc *c_proc, int reg_nb,
+	unsigned int add);
+int					add_ind_reg(unsigned char *board, t_proc *c_proc, int v,
+	int *reg_nb);
+int					add_reg_reg(unsigned char *board, t_proc *c_proc, int v,
+	int *reg_nb);
+void				cmd_fork(unsigned char *board, t_proc *c_proc,
+	t_dbllist *pr_list, t_cor *core);
+void				cmd_lld(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_lldi(unsigned char *board, t_proc *c_proc, t_cor *core);
+void				cmd_lfork(unsigned char *board, t_proc *c_proc,
+	t_dbllist *pr_list, t_cor *core);
+void				cmd_aff(unsigned char *board, t_proc *c_proc, t_cor *core);
 
 /*
 **********************  Game Loop **********************
 */
 
-void			init(t_cor *core, t_options *options);
-int				game_loop(t_cor *core, t_dbllist *ch_list, t_dbllist *pr_list);
-void			verbose_cycles(t_cor *core);
-int				execute_dead_process(t_dbllist **pr_list, t_cor *core);
-int				iscmd(t_proc *c_proc, t_cor *core, t_dbllist *ch_list,
+void				init(t_cor *core, t_options *options);
+int					game_loop(t_cor *core, t_dbllist *ch_list,
 	t_dbllist *pr_list);
-void			check_cmd(t_cor *core, t_dbllist **ch_list,
+void				verbose_cycles(t_cor *core);
+int					execute_dead_process(t_dbllist **pr_list, t_cor *core);
+int					iscmd(t_proc *c_proc, t_cor *core, t_dbllist *ch_list,
+	t_dbllist *pr_list);
+void				check_cmd(t_cor *core, t_dbllist **ch_list,
 	t_dbllist **pr_list);
-
 
 /*
 **********************  Ncurses **********************
 */
 
-int				set_up_ncurses(t_cor *core);
-int 			print_board(t_cor *core, t_dbllist *process_list, unsigned char *board);
-int				ncurs_finish(t_cor *core);
-
+int					set_up_ncurses(t_cor *core);
+int					print_board(t_cor *core, t_dbllist *process_list,
+	unsigned char *board);
+int					ncurs_finish(t_cor *core);
 
 #endif
--- VM/op.h
+++ VM/op.h
@@ -11,88 +11,50 @@
 /* ************************************************************************** */
 
 #ifndef OP_H
-#define OP_H
+# define OP_H
+# define IND_SIZE			2
+# define REG_SIZE			4
+# define DIR_SIZE			REG_SIZE
 
-/*
-** Toutes les tailles sont en octets.
-** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
-*/
+# define REG_CODE			1
+# define DIR_CODE			2
+# define IND_CODE			3
 
-#define IND_SIZE				2
-#define REG_SIZE				4
-#define DIR_SIZE				REG_SIZE
+# define MAX_ARGS_NUMBER	4
+# define MAX_PLAYERS		4
+# define MEM_SIZE			(4*1024)
+# define IDX_MOD			(MEM_SIZE / 8)
+# define CHAMP_MAX_SIZE		(MEM_SIZE / 6)
 
+# define COMMENT_CHAR		'#'
+# define LABEL_CHAR			':'
+# define DIRECT_CHAR		'%'
+# define SEPARATOR_CHAR		','
 
-# define REG_CODE				1
-# define DIR_CODE				2
-# define IND_CODE				3
+# define LABEL_CHARS		"abcdefghijklmnopqrstuvwxyz_0123456789"
 
+# define NAME_CMD_STRING	".name"
+# define COMMENT_CMD_STRING	".comment"
 
-#define MAX_ARGS_NUMBER			4
-#define MAX_PLAYERS				4
-#define MEM_SIZE				(4*1024)
-#define IDX_MOD					(MEM_SIZE / 8)
-#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+# define REG_NUMBER			16
 
-#define COMMENT_CHAR			'#'
-#define LABEL_CHAR				':'
-#define DIRECT_CHAR				'%'
-#define SEPARATOR_CHAR			','
+# define CYCLE_TO_DIE		1536
+# define CYCLE_DELTA		50
+# define NBR_LIVE			21
+# define MAX_CHECKS			10
 
-#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+# define T_REG				1
+# define T_DIR				2
+# define T_IND				4
+# define T_LAB				8
 
-#define NAME_CMD_STRING			".name"
-#define COMMENT_CMD_STRING		".comment"
-
-#define REG_NUMBER				16
-
-#define CYCLE_TO_DIE			1536
-#define CYCLE_DELTA				50
-#define NBR_LIVE				21
-#define MAX_CHECKS				10
-
-/*
-**
-*/
-
-typedef char	t_arg_type;
-
-#define T_REG					1
-#define T_DIR					2
-#define T_IND					4
-#define T_LAB					8
-
-/*
-**
-*/
-
-# define PAD_T (16)
-# define EXEC_MAGIC_LENGTH (4)
-# define PAD (4)
-# define COR_MIN_VALUE			((PROG_NAME_LENGTH + COMMENT_LENGTH + PAD_T) * 2)
-# define COR_MAX_VALUE			((COR_MIN_VALUE + (CHAMP_MAX_SIZE) * 2))
-# define PROG_NAME_LENGTH		(128)
-# define COMMENT_LENGTH			(2048)
-# define COREWAR_EXEC_MAGIC		0xea83f3
-
-typedef struct		header_s
-{
-  unsigned int		magic;
-  char				prog_name[PROG_NAME_LENGTH + 1];
-  unsigned int		prog_size;
-  char				comment[COMMENT_LENGTH + 1];
-}					header_t;
-
-typedef struct		s_op
-{
-	char			*name;
-	int				nb_param;
-	t_arg_type		arg[MAX_ARGS_NUMBER];
-	int				op_code;
-	int				nb_cycle;
-	char			*comment;
-	int				codage_oct;
-	int				unknown;
-}					t_op;
+# define PAD_T				(16)
+# define EXEC_MAGIC_LENGTH	(4)
+# define PAD				(4)
+# define COR_MIN_VALUE		((PROG_NAME_LENGTH + COMMENT_LENGTH + PAD_T) * 2)
+# define COR_MAX_VALUE		((COR_MIN_VALUE + (CHAMP_MAX_SIZE) * 2))
+# define PROG_NAME_LENGTH	(128)
+# define COMMENT_LENGTH		(2048)
+# define COREWAR_EXEC_MAGIC	0xea83f3
 
 #endif
