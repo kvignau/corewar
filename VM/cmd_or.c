db729bf23a1b293fcb75e24e15dd52a4fd393d11
Alexandre FOLLIN <afollin@e2r8p4.42.fr>
2016-12-02 08:50:52 +0100

    and or xor pour de vrai susu

--- VM/cmd_or.c
+++ VM/cmd_or.c
@@ -12,98 +12,48 @@
 
 #include "corewar.h"
 
-static void		norme_de_m2rde(unsigned char *board, t_proc *c_proc,
-	int *reg, int i[])
-{
-	if (((board[(c_proc->i + 1) % MEM_SIZE]) & 0b00001100) >> 2 == 0b01)
-		*reg = get_register_name(board, c_proc, i);
-	else
-		i[1] = -1;
-}
-
-static void		norme_de_m1rde(t_proc *c_proc, int arg1, int arg2, int reg)
-{
-	c_proc->r[reg - 1] = arg1 | arg2;
-	c_proc->carry = c_proc->r[reg - 1] == 0 ? 1 : 0;
-}
-
-static void		norme_de_m0rde(int i[], int *o_code, unsigned char *board,
-	t_proc *c_proc)
-{
-	i[0] = 2;
-	i[1] = 0;
-	*o_code = board[(c_proc->i + 1) % MEM_SIZE];
-}
-
-void		cmd_or(unsigned char *b, t_proc *c_proc, t_cor *core)
+void		init_aox_o(t_aox *aox, t_proc *c_proc, t_cor *core, int i[])
 {
 	int		o_code;
-	int		arg1;
-	int		arg2;
-	int		reg;
+
+	o_code = core->board[(c_proc->i + 1) % MEM_SIZE];
+	aox->arg1 = get_arg_val((o_code & 0b11000000) >> 6, core->board, c_proc, i);
+	aox->arg2 = get_arg_val((o_code & 0b00110000) >> 4, core->board, c_proc, i);
+	if ((o_code & 0b00001100) >> 2 == 0b01)
+		aox->r_dest = get_register_name(core->board, c_proc, i);
+	else
+		i[1] = -1;
+	if ((o_code & 0b00000011) != 0b00)
+		i[1] = -2;
+	if (i[1] == 0)
+	{
+		c_proc->r[aox->r_dest - 1] = aox->arg1 | aox->arg2;
+		c_proc->carry = c_proc->r[aox->r_dest - 1] == 0 ? 1 : 0;
+	}
+}
+
+void		cmd_or(unsigned char *board, t_proc *c_proc, t_cor *core)
+{
+	t_aox	aox;
 	int		i[2];
 
-	norme_de_m0rde(i, &o_code, b, c_proc);
+	i[0] = 2;
+	i[1] = 0;
 	if (c_proc->ctp == 6)
 	{
-		arg1 = get_arg_val((o_code & 0b11000000) >> 6, b, c_proc, i);
-		arg2 = get_arg_val((o_code & 0b00110000) >> 4, b, c_proc, i);
-		norme_de_m2rde(b, c_proc, &reg, i);
-		if ((o_code & 0b00000011) != 0b00)
-			i[1] = -1;
-		if (i[1] == 0)
-			norme_de_m1rde(c_proc, arg1, arg2, reg);
+		init_aox_o(&aox, c_proc, core, i);
 		if (core->options.verbose == 1)
 		{
 			if (i[1] == 0)
-				ft_printf("P% 5d | or %d %d r%d\n", c_proc->pid, arg1, arg2, reg);
-			cmd_verbose(b, c_proc, get_cmd_size(get_type(b, c_proc), 4, 3));
+				ft_printf("P% 5d | or %d %d r%d\n", c_proc->pid, aox.arg1,
+					aox.arg2, aox.r_dest);
+			cmd_verbose(board, c_proc, (get_cmd_size(get_type(board, c_proc),
+				4, 3)));
 		}
 		c_proc->c_cmd = 0;
-		next_pc(get_cmd_size(get_type(b, c_proc), 4, 3), c_proc, b);
+		next_pc(get_cmd_size(get_type(board, c_proc), 4, 3), c_proc, board);
 		c_proc->ctp = 1;
 	}
 	else
 		c_proc->ctp += 1;
 }
-
-// void		cmd_or(unsigned char *board, t_proc *c_proc, t_cor *core)
-// {
-// 	int		o_code;
-// 	int		arg1;
-// 	int		arg2;
-// 	int		r_dest;
-// 	int		i[2];
-
-// 	i[0] = 2;
-// 	i[1] = 0;
-// 	o_code = board[(c_proc->i + 1) % MEM_SIZE];
-// 	if (c_proc->ctp == 6)
-// 	{
-// 		arg1 = get_arg_val((o_code & 0b11000000) >> 6, board, c_proc, i);
-// 		arg2 = get_arg_val((o_code & 0b00110000) >> 4, board, c_proc, i);
-// 		if ((o_code & 0b00001100) >> 2 == 0b01)
-// 			r_dest = get_register_name(board, c_proc, i);
-// 		else
-// 			i[1] = -1;
-// 		if ((o_code & 0b00000011) != 0b00)
-// 			i[1] = -2;
-// 		if (i[1] == 0)
-// 		{
-// 			c_proc->r[r_dest - 1] = arg1 | arg2;
-// 			c_proc->carry = c_proc->r[r_dest - 1] == 0 ? 1 : 0;
-// 		}
-// 		if (core->options.verbose == 1)
-// 		{
-// 			if (i[1] == 0)
-// 				ft_printf("P% 5d | or %d %d r%d\n", c_proc->pid, arg1, arg2,
-// 					r_dest);
-// 			cmd_verbose(board, c_proc, (get_cmd_size(get_type(board, c_proc), 4, 3)));
-// 		}
-// 		c_proc->c_cmd = 0;
-// 		next_pc(get_cmd_size(get_type(board, c_proc), 4, 3), c_proc, board);
-// 		c_proc->ctp = 1;
-// 	}
-// 	else
-// 		c_proc->ctp += 1;
-// }
