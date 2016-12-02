ae6278e593bc2e2e1b4982cf9f44e3b183163877
Michael CHEVALLIER <mchevall@e2r9p5.42.fr>
2016-12-02 07:11:11 +0100

    norme get_cmd_size

--- VM/get_cmd_size.c
+++ VM/get_cmd_size.c
@@ -12,6 +12,20 @@
 
 #include "corewar.h"
 
+int		param_3(int nb_param, int *type, int label_size, int size)
+{
+	if (nb_param == 3)
+	{
+		if (type[2] == REG)
+			size += 1;
+		else if (type[2] == DIR)
+			size += label_size;
+		else if (type[2] == IND)
+			size += 2;
+	}
+	return (size);
+}
+
 int		get_cmd_size(int *type, int label_size, int nb_param)
 {
 	int		size;
@@ -35,15 +49,7 @@ int		get_cmd_size(int *type, int label_size, int nb_param)
 		else if (type[1] == IND)
 			size += 2;
 	}
-	if (nb_param == 3)
-	{
-		if (type[2] == REG)
-			size += 1;
-		else if (type[2] == DIR)
-			size += label_size;
-		else if (type[2] == IND)
-			size += 2;
-	}
+	size = param_3(nb_param, type, label_size, size);
 	free(type);
 	return (size);
 }
